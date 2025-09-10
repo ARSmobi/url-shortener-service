ebf57caff160e4c468f2e7bcc2258b1bc70ab808
ARSmobi <ARSmobi>
2025-09-10 23:28:12 +0300

    Add authorization with JWT. Changed woraround user-id to current_user in /links(get, post)

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+SECRET_KEY = "6ce314fff41e915cd2bbc42acc200e2528f634ce3402a6036a006285cf9a522e"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Настройка хеширования паролей алгоритмом bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -10,3 +16,19 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Проверяет соответствие хеша и пароля"""
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    """Создает JWT-токен"""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    """Проверяет JWT-токен"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from cgitb import reset
 
 from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.future import select
@@ -8,6 +9,7 @@ from sqlalchemy.future import select
 from app import models, schemas, crud
 from app.database import engine, get_db
 from app.utils import generate_short_url
+from app.auth import verify_token, create_access_token, verify_password
 
 app = FastAPI(title="URL Shortener", version="0.1.0")
 
@@ -25,61 +27,73 @@ async def root():
 # Запуск для разработки: uvicorn app.main:app --reload
 
 # Эндпоинт для регистрации нового пользователя
-# @app.post("/register", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
-# async def register_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
-#     """Создание нового пользователя"""
-#     db_user = await crud.get_user_by_email(db=db, email=user.email)
-#     if db_user:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail="Пользователь с таким email уже существует"
-#         )
-#
-#     try:
-#         new_user = await crud.create_user(db, user)
-#         return new_user
-#     except ValueError as e:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail=str(e)
-#         )
-
 @app.post("/register", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
 async def register_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
-    """
-    Создает нового пользователя в системе.
-    """
-    try:
-        # Пытаемся создать пользователя. Если email не уникален, БД выбросит IntegrityError.
-        new_user = await crud.create_user(db=db, user=user)
-        return new_user
+    """Создание нового пользователя"""
+    db_user = await crud.get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Пользователь с таким email уже существует"
+        )
 
-    except IntegrityError as e:
-        # Ловим конкретное исключение о нарушении целостности данных (уникальности)
-        await db.rollback()  # Важно: откатываем failed transaction
-        # Проверяем, что ошибка именно про уникальность email
-        if "unique" in str(e).lower() and "email" in str(e).lower():
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Пользователь с таким email уже существует"
-            )
-        else:
-            # Если это какая-то другая ошибка целостности, поднимаем ее с общим сообщением
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Произошла ошибка при создании пользователя"
-            )
+    try:
+        new_user = await crud.create_user(db, user)
+        return new_user
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+
+# Схема аутентификации
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_db)):
+    """Проверка токена и возврат текущего пользователя"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Неверный токен",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    # Проверка токена
+    payload = verify_token(token)
+    if not payload:
+        raise credentials_exception
+
+    # Извлекаем user_id из токена
+    user_id = payload.get("sub")
+    if not user_id:
+        raise credentials_exception
+
+    # Получение пользователя из БД
+    user = await db.get(models.User, int(user_id))
+    if not user:
+        raise credentials_exception
+
+    return user
 
 # Эндпоинт для получения короткого URL
 @app.post("/links", response_model=schemas.LinkOut)
 async def create_short_url(
         link: schemas.LinkCreate,
-        user_id: int,  #ВРЕМЕННО для теста. Передать user_id из токена
+        current_user: models.User = Depends(get_current_user),
         db: AsyncSession = Depends(get_db)):
     """Создание короткого URL для авторизованного пользователя"""
     # Заменить на настоящую проверку токена
-    new_link = await crud.create_short_url(db, link, user_id)
+    new_link = await crud.create_short_url(db=db, link=link, user_id=current_user.id)
     return new_link
+
+# Эндпоинт для получения списка ссылок пользователя
+@app.get("/links", response_model=list[schemas.LinkOut])
+async def get_user_links(
+        current_user: models.User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)):
+    """Получение списка ссылок пользователя"""
+    return await crud.get_user_links(db=db, user_id=current_user.id)
 
 # Эндпоинт для редиректа по короткому URL
 @app.get("/r/{short_url}")
@@ -102,3 +116,21 @@ async def redirect_to_original_url(short_url: str, db: AsyncSession = Depends(ge
     # Перенаправление на оригинальный URL
     from fastapi.responses import RedirectResponse
     return RedirectResponse(url=link.original_url)
+
+# Эндпоинт для получения информации о пользователе и аутентификации
+@app.post("/token")
+async def login_for_access_token(
+        from_data: OAuth2PasswordRequestForm = Depends(),
+        db: AsyncSession = Depends(get_db)):
+    """Аутентификация пользователя и получение токена"""
+    user = await crud.get_user_by_email(db=db, email=from_data.username)
+    if not user or not verify_password(from_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный логин или пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # Получение токена с данными пользователя
+    access_token = create_access_token(data={"sub": str(user.id)})
+
+    return {"access_token": access_token, "token_type": "bearer"}
